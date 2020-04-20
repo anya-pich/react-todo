bdba5267e2d1c4a197b3f0eda1381c0f7f6e1880
anya-pich <anna.pich@outlook.com>
2020-04-20 14:30:53 -0700

    sprint 5

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TodoModel from '../models/Todo';
 import Todos from '../components/Todos';
+import CreateTodoForm from '../components/CreateTodoForm';
 
 class TodosContainer extends Component {
 	constructor() {
@@ -9,9 +10,11 @@ class TodosContainer extends Component {
 			todos: [],
 		};
 	};
+
 	componentDidMount() {
 		this.fetchData();
 	};
+
 	fetchData = () => {
 		TodoModel.all().then((res) => {
 			this.setState ({
@@ -19,9 +22,24 @@ class TodosContainer extends Component {
 			});
 		});
 	};
+
+	createTodo = (todo) => {
+		let newTodo = {
+			body: todo,
+			completed: false,
+		};
+
+		TodoModel.create(newTodo).then((res) => {
+			let todos = this.state.todos;
+			todos.push(res.data);
+			this.setState({todos: todos});
+		})
+	}
+
   render() {
     return (
       <div className="todosComponent">
+				<CreateTodoForm createTodo={this.createTodo} />
 				<Todos todos={this.state.todos} />
       </div>
     );
